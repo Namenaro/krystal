4bce5ceede3816b5117818e7e1a8791cea6c0657
namenaro <nanenaro@gmail.com>
2023-07-27 16:50:12 +0300

    tughi

--- situation.py
+++ situation.py
@@ -28,10 +28,30 @@ class Situation:
         return parent_name, grower
 
     def get_worst_point(self):
-        return index
+        point_err_worst = None
+        point_index_worst = None
+        for point in range(len(self.full_signal)):
+            latest_cristall = self.get_best_cristall_for_point(point)
+            err_in_point = latest_cristall.get_err_in_point(point)
+
+            if point_err_worst is None:
+                point_err_worst = err_in_point
+                point_index_worst = point
+            else:
+                if point_err_worst< err_in_point:
+                    point_err_worst = err_in_point
+                    point_index_worst = point
+
+        return point_index_worst
 
     #-----------------------------------------------------------
     #----------------------------------------------------------
+    def get_best_cristall_for_point(self, point):
+        names = self.points_to_cristalls_names[point]
+        if len(names) == 0:
+            return None
+        best_cristall_name = names[-1]
+        return self.names_to_crystalls[best_cristall_name]
 
     def register_crystall_no_modification(self, cristall):
         cristal_id = self.names_gen.generate_id()
